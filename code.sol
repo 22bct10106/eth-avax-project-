0dc6aeef252e050454f378f2716283e0c852e0fc
22bct10106 <138612265+22bct10106@users.noreply.github.com>
2023-08-20 18:18:38 +0530

    Update code.sol

--- code.sol
+++ code.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract RequireAssertRevertExample {
+contract errorHandeling {
     uint256 public value;
 
     function setValue(uint256 newValue) external {
